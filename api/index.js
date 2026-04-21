30f42d6e5177417ba3416cd887ca7468c35bb0cc
douglas morett <dougmorett@gmail.com>
2026-04-21 17:46:13 -0300

    Deploy final de emergencia: Marcha total

---
+++ api/index.js
@@ -0,0 +1,18 @@
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  return res.status(200).json({ 
+    status: 'success', 
+    user: { 
+      id: 1, 
+      name: 'Douglas Hakim', 
+      email: 'douglas@firecheck.com', 
+      role: 'admin', 
+      store: 'Loja Matriz' 
+    } 
+  });
+};
