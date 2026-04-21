8af0dc2267c0c38c420deebd3f4914c0f76dd576
douglas morett <dougmorett@gmail.com>
2026-04-21 17:47:00 -0300

    Restauração completa de arquitetura ESM

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -15,4 +15,4 @@ module.exports = (req, res) => {
       store: 'Loja Matriz' 
     } 
   });
-};
+}
